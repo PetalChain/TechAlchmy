c14a8152049fecb6afd1e28e5f796185849834c6
TechAlchmy <antoncatalinspr77@gmail.com>
2024-08-22 16:44:37 +0800

    techalchmy

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,7 @@ const overlay = document.querySelector("[data-overlay]");
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
+const modalPosition = document.querySelector("[data-modal-position]");
 
 // modal toggle function
 const testimonialsModalFunc = function () {
@@ -42,6 +43,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
     modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalPosition.innerHTML = this.querySelector("[data-testimonials-position]").innerHTML;
 
     testimonialsModalFunc();
 
@@ -50,8 +52,8 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn?.addEventListener("click", testimonialsModalFunc);
+overlay?.addEventListener("click", testimonialsModalFunc);
 
 
 
@@ -61,7 +63,7 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+select?.addEventListener("click", function () { elementToggleFunc(this); });
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
@@ -139,11 +141,10 @@ for (let i = 0; i < formInputs.length; i++) {
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
-
+console.log(navigationLinks.length)
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
@@ -157,3 +158,138 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+//////////////flying-animation////////////////////
+// Get the container element
+const container = document.querySelector('.about');
+
+let flies = [];
+// Function to create a new fly element
+function createFly() {
+  // Create a new fly element
+  const fly = document.createElement('div');
+  fly.classList.add('fly');
+
+  // Set the initial position and opacity
+  let left = Math.floor(Math.random() * (container.offsetWidth - 20));
+  fly.style.left = `${left}px`;
+  let top = container.offsetHeight + 50;
+  fly.style.top = `${top}px`;
+  fly.style.opacity = 1;
+
+  let size = 5 + Math.random() * 10;
+  fly.style.width = fly.style.height = `${size}px`
+  fly.style.filter = `blur(${size}px)`
+  // Add the fly element to the container
+  container.appendChild(fly);
+  flies.push({
+    element: fly,
+    left: left, 
+    top: top,
+    opacity: 1,
+    size:size,
+    speed: (25-size) / 10,
+    speedX: Math.random() > 0.5 ? 0.5 : -0.5,
+  });
+}
+
+function moveFly() {
+  for(let i = 0; i < flies.length; i ++) {
+    flies[i].top -= flies[i].speed;
+    flies[i].element.style.top = `${flies[i].top}px`
+
+    flies[i].left -= flies[i].speedX;
+    flies[i].speedX -= flies[i].speedX / Math.abs(flies[i].speedX) * (25-flies[i].size) / 10000
+    flies[i].element.style.left = `${flies[i].left}px`
+
+    flies[i].opacity -= 0.005;
+    flies[i].element.style.opacity = flies[i].opacity
+
+    if(flies[i].opacity < 0) {
+      flies[i].element.remove();
+      flies.splice(i, 1);
+    }
+  }
+}
+
+setInterval(createFly, 30)
+setInterval(moveFly, 10)
+
+
+/////////// Position Animation ///////////////////////////
+const devPosition = document.querySelector('.dev-position');
+
+const positions =["React | Next", "Laravel | Vue", "Web3", "Mobile App", "Svelte | SvelteKit", ];
+let word_index = 0;
+let letter_index = 0;
+let direct = 1;
+let pause = 0;
+let isPause = 0;
+function animation() {
+  devPosition.innerHTML = positions[word_index].slice(0, letter_index);
+  if(isPause == 0)
+    letter_index += direct;
+  else{
+    pause ++;
+    if(pause == 10) {
+      isPause = 0;
+    }
+  }
+  if(isPause == 0 && direct == 1 && letter_index == positions[word_index].length) {
+    direct = -1;
+    isPause = 1;
+    pause = 0;
+  }
+  if(letter_index == 0) {
+    direct = 1;
+    word_index = (word_index + 1) % positions.length
+  }
+}
+
+setInterval(animation, 80)
+
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  grabCursor: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+  const progressContent = document.querySelector(".autoplay-progress span");
+  var swiper = new Swiper(".mySwiperPic", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress) {
+        progressCircle.style.setProperty("--progress", 1 - progress);
+        progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    }
+  });
